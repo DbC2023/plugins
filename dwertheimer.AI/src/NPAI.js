8a57211fbedbcec5410dbd2c11b829baa2e3ff78
David Wertheimer <dwertheimer@users.noreply.github.com>
2022-12-05 01:03:48 -0800

    Initial commit of test content

---
+++ dwertheimer.AI/src/NPAI.js
@@ -0,0 +1,151 @@
+// @flow
+
+import pluginJson from '../plugin.json'
+import { chooseOption, showMessage } from '@helpers/userInput'
+import { log, logDebug, logError, logWarn, clo, JSP, timer } from '@helpers/dev'
+
+const baseURL = 'https://api.openai.com/v1'
+const modelsComponent = 'models'
+const imagesGenerationComponent = 'images/generations'
+
+/**
+ * Format a Fetch request object for the OpenAI API, including the Authorization header and the contents of the post if any
+ * @param {string} method - GET, POST, PUT, DELETE
+ * @param {string} body - JSON string to send with POST or PUT
+ * @returns
+ */
+export const getRequestObj = (method: string = 'GET', body: any = null): any => {
+  const { apiKey } = DataStore.settings
+  if (apiKey.length) {
+    const obj = {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${apiKey}`,
+      },
+    }
+    if (body && method !== 'GET') {
+      // $FlowFixMe
+      obj.body = JSON.stringify(body)
+    }
+    clo(obj, 'getRequestObj returning:')
+    return obj
+  } else {
+    showMessage('Please set your API key in the plugin settings')
+    logError(pluginJson, 'No API Key found')
+  }
+}
+
+/**
+ * Make a request to the GPT API
+ * @param {string} component - the last part of the URL (after the base URL), e.g. "models" or "images/generations"
+ * @param {string} requestType - GET, POST, PUT, etc.
+ * @param {string} data - body of a POST/PUT request - can be an object (it will get stringified)
+ * @returns {any|null} JSON results or null
+ */
+export async function makeRequest(component: string, requestType: string = 'GET', data: any = null): any | null {
+  const url = `${baseURL}/${component}`
+  logDebug(pluginJson, `makeRequest: about to fetch ${url}`)
+  const result = await fetch(url, getRequestObj(requestType, data))
+  if (result) {
+    clo(result, `makeRequest() result of fetch to: "${url}"`)
+    const resultJSON = JSON.parse(result)
+    if (resultJSON?.data) {
+      return resultJSON.data
+    } else if (resultJSON.error) {
+      logError(pluginJson, `makeRequest received error: ${JSP(resultJSON.error)}`)
+      await showMessage(`GPT API Returned Error: ${resultJSON.error.message}`)
+    }
+    return null
+  }
+  return null
+}
+
+/**
+ * Get the model list from OpenAI and ask the user to choose one
+ * @returns {string|null} the model ID chosen
+ */
+export async function chooseModel(): Promise<string | null> {
+  const models = await makeRequest(modelsComponent)
+  if (models) {
+    const modelOptions = models.map((model) => ({ label: model.id, value: model.id }))
+    return await chooseOption('Choose a model', modelOptions)
+  } else {
+    logError(pluginJson, 'No models found')
+  }
+  return null
+}
+
+/**
+ * Ask for a prompt and n results from user
+ * @returns { prompt: string, n: number }
+ */
+export async function getPromptAndNumberOfResults(promptIn: string | null = null, nIn: number | null = null): { prompt: string, n: number } {
+  const prompt = promptIn ?? (await CommandBar.showInput('Enter a prompt'))
+  const n = nIn ?? (await CommandBar.showInput('Enter the number of results to generate'))
+  return { prompt, n: parseInt(n) }
+}
+
+/**
+ * Create a request object for DALL-E image request
+ * https://beta.openai.com/docs/api-reference/images/create
+ * @param {string} prompt - A text description of the desired image(s). The maximum length is 1000 characters.
+ * @param {number} numberOfResults - The number of images to generate. Must be between 1 and 10
+ * @param {string} size - The size of the generated images. Must be one of 256x256, 512x512, or 1024x1024.
+ * @param {string} response_format - The format in which the generated images are returned. Must be one of url or b64_json
+ * @param {string} user - A unique identifier representing your end-user, which can help OpenAI to monitor and detect abuse.
+ */
+export function createImageRequestBody(prompt: string, n: number = 10, size: string = '1024x1024', response_format: string = 'url', user: string = null) {
+  const obj = { prompt, n, size, response_format }
+  if (user) obj.user = user
+  return obj
+}
+
+/**
+ * test connection to GPT API by getting models list and making a request for an image
+ * Plugin entrypoint for command: "/COMMAND"
+ * @param {*} incoming
+ */
+export async function testConnection(model: string | null = null) {
+  try {
+    logDebug(pluginJson, `testConnection running with model:${String(model)}`)
+
+    let chosenModel = model
+    // get models/engines (to choose pricing/capability)
+    if (!model) {
+      chosenModel = await chooseModel()
+    }
+    if (chosenModel) {
+      clo(chosenModel, 'testConnection chosenModel')
+    } else {
+      logWarn(pluginJson, 'No model chosen')
+    }
+  } catch (error) {
+    logError(pluginJson, JSP(error))
+  }
+}
+
+/**
+ * test connection to GPT API by getting models list and making a request for an image
+ * Plugin entrypoint for command: "/COMMAND"
+ * @param {*} incoming
+ */
+export async function createAIImages(promptIn: string | null = null, nIn: number = 10, sizeIn: string = '1024x1024', response_formatIn: string = 'url', userIn: string = '') {
+  try {
+    // logDebug(pluginJson, `createImages running with prompt:${String(prompt) ${nIn} ${sizeIn} ${response_formatIn} ${userIn}`)
+
+    // get an image
+    const start = new Date()
+    const { prompt, n } = await getPromptAndNumberOfResults(promptIn, nIn)
+    const request = await makeRequest(imagesGenerationComponent, 'POST', createImageRequestBody(prompt, n, sizeIn, response_formatIn))
+    const time = timer(start)
+    clo(request, `testConnection imageRequest result`)
+    if (request) {
+      const msg = `Call to DALL-E took ${time}. ${request.length} results for "${prompt}":`
+      Editor.insertTextAtCursor(msg)
+      request.forEach((r, i) => Editor.insertTextAtCursor(`[Result${i}](${r.url})`))
+    }
+  } catch (error) {
+    logError(pluginJson, JSP(error))
+  }
+}
